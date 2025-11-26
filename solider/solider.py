7106ad04400ebdb0c64b68dd0a5b3874a34b9297
mendy hershkop <hers.mm@gmail.com>
2025-11-26 11:14:01 +0200

    create a solider classes

---
+++ solider/solider.py
@@ -0,0 +1,9 @@
+class solider:
+    def __init__(self,id:int,first_name:str,least_name:str, gender:str,citi:str, far_from_base:int, status:bool):
+        self.id=id
+        self.first_name=first_name
+        self.least_name=least_name
+        self.gender=gender
+        self.citi=citi
+        self.far_from_base=far_from_base
+        self.status=status
