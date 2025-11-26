041e33b1cc9d9263afa48fe2e23ed8e0ffead9c0
mendy hershkop <hers.mm@gmail.com>
2025-11-26 13:03:47 +0200

    second version of server

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 import codecs
+from solider.solider import Solider
 
 from fastapi import FastAPI, HTTPException, UploadFile, File
 import csv
 import uvicorn
 
 app=FastAPI()
-
+def create_solider(soliderCsv):
+    soliders=[]
+    for i in list(soliderCsv):
+        soilder=Solider(i['מספר אישי'], i['שם פרטי'], i['שם משפחה'], i['מין'], i['עיר מגורים'], i['מרחק מהבסיס'])
+        soliders.append(soilder)
+    return soliders
+def order_by_far(solider_list):
+    solider=create_solider(solider_list)
+    solider.sorted(solider[far_from_base])
+    return solider
 @app.get("/")
 def main_page():
     return {"massage":"welcome to base Shibulim room meaning "}
@@ -14,13 +24,5 @@ async def assign_csv(file: UploadFile = File(...)):
     if not file.filename.endswith('.csv'):
         raise HTTPException(status_code=400, detail="File must be a CSV file")
     csvReader = csv.DictReader(codecs.iterdecode(file.file, 'utf-8'))
-    # data = {}
-    # for rows in csvReader:
-    #     key = rows['מספר אישי']
-    #     data[key] = rows
-    #
-    # file.file.close()
-    for i in
-    return list(csvReader)
-
+    return order_by_far(list(csvReader))
 
